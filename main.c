ae7c5b10cc82448a4ea6d022ebb760949ed56ac5
Stéphane du Hamel <stephduh@live.fr>
2023-09-14 14:25:19 +0200

    move output logic to new file

--- main.c
+++ main.c
@@ -30,17 +30,24 @@ void usleep(__int64 usec)
 #define PX_ASPECT 2.25
 #endif
 
+
+#ifndef ASPECT_RATIO
+#define ASPECT_RATIO 1
+#endif
+
 #ifndef HEIGHT
 #define HEIGHT 64
 #endif
 #ifndef WIDTH
-#define WIDTH ((int)(HEIGHT * PX_ASPECT))
+#define WIDTH ((int)(HEIGHT * PX_ASPECT* ASPECT_RATIO))
 #endif
 
 #define PERSPECTIVE_UV // enable perspective texture mapping
 
 #define RASTERM_IMPLEMENTATION
 #include "rasterm.h"
+
+#include "printImg.h"
 
 void chessboardShader(float bufferColor[4], int x, int y, Vector2D uv, float inverseDepth, SurfaceAttributes attribs, SceneAttributes scene)
 {
@@ -73,7 +80,7 @@ void from_obj(float buffer[WIDTH][HEIGHT][4], int t)
 
     SceneAttributes scene = {lightVec, (Vector3D){.999, .999, .995}, (Vector3D){.005, .005, .01}, camera};
 
-// #pragma omp parallel for
+    // #pragma omp parallel for
     for (int f = 0; f < faces_count_pot; f++)
     {
         const float rotateXY = 0.4;
@@ -142,150 +149,6 @@ void from_obj(float buffer[WIDTH][HEIGHT][4], int t)
 #ifndef FRAMES
 #define FRAMES 1
 #endif
-
-char *ascii = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
-int asciiLen = 92;
-
-char *color = "\033";
-
-const char *colors[8] = {"[1;30m", "[1;31m", "[1;32m", "[1;33m", "[1;34m", "[1;35m", "[1;36m", "[1;37m"};
-const char *bgcolors[8] = {"[1;40m", "[1;41m", "[1;42m", "[1;43m", "[1;44m", "[1;45m", "[1;46m", "[1;47m"};
-const int black = 0;
-const int red = 1;
-const int green = 2;
-const int yellow = 3;
-const int blue = 4;
-const int magenta = 5;
-const int cyan = 6;
-const int white = 7;
-
-char *defaultColor = "[0m";
-
-float lumaR = 0.2126;
-float lumaG = 0.7152;
-float lumaB = 0.0722;
-
-float bayer(int x, int y, int level)
-{
-    if (level <= 0)
-    {
-        x &= 1;
-        y &= 1;
-        return x == 0 ? (y == 0 ? 0 : .5f) : (y == 0 ? .75f : .25f);
-    }
-    return bayer(x / 2, y / 2, level - 1) * .25f + bayer(x, y, 0);
-}
-
-Vector2D toCocg(Vector3D rgb)
-{
-    float co = rgb.x - rgb.z;
-    float tmp = rgb.z + co / 2;
-    float cg = rgb.y - tmp;
-    return (Vector2D){co, cg};
-}
-
-void print_color(float buffer[WIDTH][HEIGHT][4])
-{
-    int termColorBuffer[WIDTH][HEIGHT][2];
-    #pragma omp parallel for
-    for (int i = 0; i < WIDTH; i++)
-    {
-        for (int j = 0; j < HEIGHT; j++)
-        {
-            float db = bayer(i, j, 4);
-            float dr = bayer(i, j, 5);
-            float dg = bayer(i, j, 6);
-
-            float r = buffer[i][j][0];
-            r = r < 0. ? 0. : r;
-            float g = buffer[i][j][1];
-            g = g < 0. ? 0. : g;
-            float b = buffer[i][j][2];
-            b = b < 0. ? 0. : b;
-            float luma = (r * lumaR + g * lumaG + b * lumaB);
-            if (luma == 0)
-            {
-                termColorBuffer[i][j][0] = 0;
-                termColorBuffer[i][j][1] = 0;
-                continue;
-            }
-
-            Vector2D chroma = toCocg((Vector3D){r / luma, g / luma, b / luma});
-            Vector2D chromaR = toCocg((Vector3D){1, 0, 0});
-            Vector2D chromaG = toCocg((Vector3D){0, 1, 0});
-            Vector2D chromaB = toCocg((Vector3D){0, 0, 1});
-
-            Vector2D rg = barycentric(chroma, chromaR, chromaG, chromaB);
-
-            int colorFlag = 0;
-
-            if (rg.x > dr)
-            {
-                colorFlag |= 1;
-            }
-            if (rg.y > dg)
-            {
-                colorFlag |= 2;
-            }
-            if (1 - rg.x - rg.y > db)
-            {
-                colorFlag |= 4;
-            }
-            const int lut[8] = {white, red, green, yellow, blue, magenta, cyan, white};
-            const float lum[8] = {1, lumaR, lumaG, lumaG + lumaR, lumaB, lumaB + lumaR, lumaB + lumaG, lumaB + lumaG + lumaR};
-            int charColor = lut[colorFlag];
-
-            int charid = (int)(sqrtf(luma) * asciiLen);
-            if (charid >= asciiLen)
-                charid = asciiLen - 1;
-            if (charid < 0)
-                charid = 0;
-            termColorBuffer[i][j][0] = charColor;
-            termColorBuffer[i][j][1] = charid;
-        }
-    }
-    for (int j = 0; j < HEIGHT; j++)
-    {
-        for (int i = 0; i < WIDTH; i++)
-        {
-            int charColor = termColorBuffer[i][j][0];
-            int charid = termColorBuffer[i][j][1];
-            // TODO: use string concatenation to avoid spamming stdout
-            printf("%s%s%c", color, colors[charColor], ascii[charid]);
-        }
-        printf("\n%s%s", color, defaultColor);
-    }
-}
-
-void print_grayscale(float buffer[WIDTH][HEIGHT][4])
-{
-    for (int j = 0; j < HEIGHT; j++)
-    {
-        for (int i = 0; i < WIDTH; i++)
-        {
-            float db = bayer(i, j, 4);
-            float dr = bayer(i, j, 5);
-            float dg = bayer(i, j, 6);
-
-            float r = buffer[i][j][0];
-            r = r < 0. ? 0. : r;
-            float g = buffer[i][j][1];
-            g = g < 0. ? 0. : g;
-            float b = buffer[i][j][2];
-            b = b < 0. ? 0. : b;
-
-            float luma = (r * lumaR + g * lumaG + b * lumaB);
-
-            int charid = (int)(sqrtf(luma) * asciiLen);
-            if (charid >= asciiLen)
-                charid = asciiLen - 1;
-            if (charid < 0)
-                charid = 0;
-            printf("%c", ascii[charid]);
-        }
-        printf("\n%s%s", color, defaultColor);
-    }
-}
 
 #define GRAYSCALE 0
 #define COLOR 1
