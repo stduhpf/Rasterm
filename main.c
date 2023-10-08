f6fa7fcd147f605d8f44214f620b23e085ac4dba
Stéphane du Hamel <stephduh@live.fr>
2023-10-08 02:51:54 +0200

    gdi render target

--- main.c
+++ main.c
@@ -3,7 +3,26 @@
 
 // #include <omp.h>
 
+#ifndef FRAMES
+#define FRAMES 1
+#endif
+
+#define GRAYSCALE 0
+#define COLOR 1
+#define FULL_COLOR 2
+#define GUI 3
+
+#ifndef RENDER_TARGET
+#define RENDER_TARGET GRAYSCALE
+#endif
+
 #ifdef _WIN32
+
+#if RENDER_TARGET == GUI
+#define RENDER_GUI
+#endif
+
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 void usleep(__int64 usec)
@@ -27,6 +46,7 @@ void usleep(__int64 usec)
 #include "cup.h"
 
 #ifndef PX_ASPECT
+
 #define PX_ASPECT 2.21
 #endif
 
@@ -63,7 +83,7 @@ void chessboardShader(float bufferColor[4], int x, int y, Vector2D uv, float inv
 }
 
 void print(float buffer[WIDTH][HEIGHT][4]);
-void from_obj(float* buffer, int t)
+void from_obj(float *buffer, int t)
 {
     Framebuffer fBuffer = {(float *)buffer, WIDTH, HEIGHT};
     const float cameraRotateSpeed = .01f;
@@ -146,17 +166,6 @@ void from_obj(float* buffer, int t)
         resetFragmentShader();
     }
 }
-#ifndef FRAMES
-#define FRAMES 1
-#endif
-
-#define GRAYSCALE 0
-#define COLOR 1
-#define FULL_COLOR 2
-
-#ifndef RENDER_TARGET
-#define RENDER_TARGET GRAYSCALE
-#endif
 
 void print(float buffer[WIDTH][HEIGHT][4])
 {
@@ -169,31 +178,32 @@ void print(float buffer[WIDTH][HEIGHT][4])
 #endif
 }
 
-void clearBuffer(float buffer[WIDTH][HEIGHT][4])
+void clearBuffer(Framebuffer buffer)
 {
     // #pragma omp parallel for
-    for (int i = 0; i < WIDTH; i++)
+    for (int i = 0; i < buffer.width; i++)
     {
-        for (int j = 0; j < HEIGHT; j++)
+        for (int j = 0; j < buffer.height; j++)
         {
-            buffer[i][j][0] = 0;
-            buffer[i][j][1] = 0;
-            buffer[i][j][2] = 0;
-            buffer[i][j][3] = 0;
+            float *fragment = framebufferAt(buffer, i, j);
+            fragment[0] = 0;
+            fragment[1] = 0;
+            fragment[2] = 0;
+            fragment[3] = 0;
         }
     }
 }
 
+#ifndef RENDER_GUI
 int main()
 {
 #ifdef DYNAMIC_ALLOC
     float *buffer = malloc(sizeof(float[WIDTH][HEIGHT][4]));
 #else
     float buffer[WIDTH][HEIGHT][4];
-    clearBuffer(buffer);
+    clearBuffer((Framebuffer){buffer, WIDTH, HEIGHT});
 #endif
-    // printf("reached here!\n");
-    // exit(0);
+
     for (int t = 0; t < FRAMES; t++)
     {
         from_obj(buffer, t);
@@ -201,9 +211,145 @@ int main()
         usleep(1000 * 1000 / 60);
         printf("\033[%zuA", (size_t)HEIGHT);
         printf("\033[%zuD", (size_t)WIDTH);
-        clearBuffer(buffer);
+        clearBuffer((Framebuffer){buffer, WIDTH, HEIGHT});
     }
 #ifdef DYNAMIC_ALLOC
     free(buffer);
 #endif
 }
+
+#else
+
+#ifdef _WIN32
+float *buffer;
+
+void CreateDIBAndCopyData(HDC hdc, HBITMAP *hBitmap, uint8_t **dibData)
+{
+    Framebuffer fBuffer = (Framebuffer){buffer, WIDTH, HEIGHT};
+    // Create a BITMAPINFO structure for the DIB
+    BITMAPINFO bmi;
+    ZeroMemory(&bmi, sizeof(BITMAPINFO));
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = WIDTH;
+    bmi.bmiHeader.biHeight = -HEIGHT; // Negative height to make it top-down
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32; // 32-bit (RGBA)
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    // Create a DIB section
+    *hBitmap = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, (void **)dibData, NULL, 0);
+    // Copy data from your floating-point buffer to the DIB
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            uint8_t *pixel = *dibData + (y * WIDTH + x) * 4;
+            float *fragment = framebufferAt(fBuffer, x, y);
+            float r = sqrtf(fragment[0]);
+            float g = sqrtf(fragment[1]);
+            float b = sqrtf(fragment[2]);
+            float a = fragment[3];
+
+            pixel[0] = (uint8_t)(b * 255); // Blue
+            pixel[1] = (uint8_t)(g * 255); // Green
+            pixel[2] = (uint8_t)(r * 255); // Red
+            pixel[3] = (uint8_t)(a * 255);       // Alpha
+        }
+    }
+}
+
+void PaintWindow(HWND hwnd)
+{
+
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+
+    HBITMAP hBitmap;
+    uint8_t *dibData;
+
+    // Create the DIB and copy data from the buffer
+    CreateDIBAndCopyData(hdc, &hBitmap, &dibData);
+
+    HDC memDC = CreateCompatibleDC(hdc);
+    HBITMAP hOldBitmap = (HBITMAP)SelectObject(memDC, hBitmap);
+
+    // Display the bitmap on the window
+    BitBlt(hdc, 0, 0, WIDTH, HEIGHT, memDC, 0, 0, SRCCOPY);
+
+    // Clean up
+    SelectObject(memDC, hOldBitmap);
+    DeleteObject(hBitmap);
+    DeleteDC(memDC);
+
+    EndPaint(hwnd, &ps);
+}
+
+int frame = 0;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_PAINT:
+    {
+        PaintWindow(hwnd);
+        return 0;
+    }
+    case WM_CLOSE:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    case WM_TIMER:
+    {
+        // Update the buffer for the current frame
+        clearBuffer((Framebuffer){buffer, WIDTH, HEIGHT});
+        from_obj(buffer, ++frame);
+
+        // Repaint the window to display the updated frame
+        InvalidateRect(hwnd, NULL, FALSE);
+        UpdateWindow(hwnd);
+
+        // Increment the frame index
+        return 0;
+    }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    buffer = malloc(sizeof(float[WIDTH][HEIGHT][4]));
+    from_obj(buffer, 0);
+    // Register the window class
+    WNDCLASSEX wc = {sizeof(WNDCLASSEX), CS_CLASSDC, WindowProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, ("YourWindowClass"), NULL};
+    RegisterClassEx(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(0, wc.lpszClassName, "Image Display", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, WIDTH, HEIGHT,
+                               NULL,      // Parent window
+                               NULL,      // Menu
+                               hInstance, // Instance handle
+                               NULL       // Additional application data
+    );
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+    SetTimer(hwnd, 1, 1000 / 60, NULL);
+
+    // Main message loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+        from_obj(buffer, ++frame);
+    }
+
+    // Clean up and exit
+    UnregisterClass(wc.lpszClassName, wc.hInstance);
+    return 0;
+}
+#endif // _WIN32
+#endif // RENDER_GUI
