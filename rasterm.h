e9f01a968016054ce41e9bf2a3f5bebb31c9870f
Stéphane du Hamel <stephduh@live.fr>
2023-10-20 16:31:41 +0200

    don't parallelize for small triangles

--- rasterm.h
+++ rasterm.h
@@ -299,8 +299,9 @@ void triangle2D(Framebuffer buffer, Vector2D a, Vector2D b, Vector2D c, float iz
     if (yMax < 0 || yMin >= buffer.height)
         return;
 
-#pragma omp parallel for
-    for (int x = xMin; x <= xMax; x++)
+    int x;
+#pragma omp parallel for if((xMax-xMin) > 128)
+    for (x = xMin; x <= xMax; x++)
     {
         for (int y = yMin; y <= yMax; y++)
         {
